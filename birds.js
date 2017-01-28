5cc1b6e78bb88715fab7951182f4139343f2eb19
GNUgeneration <eric@asciolla.com>
2017-01-28 17:16:08 -0500

    Make bugs.js a little simpler, and remove birds.js.

--- birds.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.waterBirds').on('click', 'button', function() {
-	$(this).closest('.waterBirds')
-	    .find('.water-birds').slideToggle();
-    });
-});
--- bugs.js
+++ bugs.js
@@ -1,46 +1,46 @@
 $(document).ready(function() {
-    $('.silverfish').on('click', 'button', function() {
-	$(this).closest('.silverfish')
+    $('.silverfish').on('click', function() {
+	$('button').closest('.silverfish')
 	    .find('.sil-answer').slideToggle();
     });
     
-    $('.slender-dipluran').on('click', 'button', function() {
+    $('.slender-dipluran').on('click', function() {
 	$(this).closest('.slender-dipluran')
 	    .find('.sle-answer').slideToggle();
     });
     
-    $('.burrowing-mayfly').on('click', 'button', function() {
+    $('.burrowing-mayfly').on('click', function() {
 	$(this).closest('.burrowing-mayfly')
 	    .find('.bur-answer').slideToggle();
     });
 
-    $('.clubtail').on('click', 'button', function() {
-	$(this).closest('.clubtail')
+    $('.clubtail').on('click', function() {
+	$('button').closest('.clubtail')
 	    .find('.clu-answer').slideToggle();
     });
 
-    $('.biddie').on('click', 'button', function() {
-	$(this).closest('.biddie')
+    $('.biddie').on('click', function() {
+	$('button').closest('.biddie')
 	    .find('.bid-answer').slideToggle();
     });
 
-    $('.white-tailed-skimmer').on('click', 'button', function() {
-	$(this).closest('.white-tailed-skimmer')
+    $('.white-tailed-skimmer').on('click', function() {
+	$('button').closest('.white-tailed-skimmer')
 	    .find('.whi-answer').slideToggle();
     });
 
-    $('.western-widow').on('click', 'button', function() {
-	$(this).closest('.western-widow')
+    $('.western-widow').on('click', function() {
+	$('button').closest('.western-widow')
 	    .find('.wes-answer').slideToggle();
     });
 
-    $('.american-ruby-spot').on('click', 'button', function() {
-	$(this).closest('.american-ruby-spot')
+    $('.american-ruby-spot').on('click', function() {
+	$('button').closest('.american-ruby-spot')
 	    .find('.ame-answer').slideToggle();
     });
 
-    $('.blackwing').on('click', 'button', function() {
-	$(this).closest('.blackwing')
+    $('.blackwing').on('click', function() {
+	$('button').closest('.blackwing')
 	    .find('.bla-answer').slideToggle();
     });
     
