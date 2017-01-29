15483de2527009f4af57dfb3767719bff29e09ea
GNUgeneration <eric@asciolla.com>
2017-01-29 07:49:14 -0500

    Simplify bugs.js still further, and test in Bugs.html.

--- bugs.js
+++ bugs.js
@@ -1,47 +1,38 @@
 $(document).ready(function() {
     $('.silverfish').on('click', function() {
-	$('button').closest('.silverfish')
-	    .find('.sil-answer').slideToggle();
+	$(this).find('.sil-answer').slideToggle();
     });
     
     $('.slender-dipluran').on('click', function() {
-	$(this).closest('.slender-dipluran')
-	    .find('.sle-answer').slideToggle();
+	$(this).find('.sle-answer').slideToggle();
     });
     
     $('.burrowing-mayfly').on('click', function() {
-	$(this).closest('.burrowing-mayfly')
-	    .find('.bur-answer').slideToggle();
+	$(this).find('.bur-answer').slideToggle();
     });
 
     $('.clubtail').on('click', function() {
-	$('button').closest('.clubtail')
-	    .find('.clu-answer').slideToggle();
+	$(this).find('.clu-answer').slideToggle();
     });
 
     $('.biddie').on('click', function() {
-	$('button').closest('.biddie')
-	    .find('.bid-answer').slideToggle();
+	$(this).find('.bid-answer').slideToggle();
     });
 
     $('.white-tailed-skimmer').on('click', function() {
-	$('button').closest('.white-tailed-skimmer')
-	    .find('.whi-answer').slideToggle();
+	$(this).find('.whi-answer').slideToggle();
     });
 
     $('.western-widow').on('click', function() {
-	$('button').closest('.western-widow')
-	    .find('.wes-answer').slideToggle();
+	$(this).find('.wes-answer').slideToggle();
     });
 
     $('.american-ruby-spot').on('click', function() {
-	$('button').closest('.american-ruby-spot')
-	    .find('.ame-answer').slideToggle();
+	$(this).find('.ame-answer').slideToggle();
     });
 
     $('.blackwing').on('click', function() {
-	$('button').closest('.blackwing')
-	    .find('.bla-answer').slideToggle();
+	$(this).find('.bla-answer').slideToggle();
     });
     
 });
